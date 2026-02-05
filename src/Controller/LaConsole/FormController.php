02b06f79dc62052a72f79987b98d0d3f6f6f809b
Rémi EECKHOUT <r.eeckhout@amiens-metropole.com>
2026-02-05 11:01:44 +0100

    feat: path changes

--- src/Controller/LaConsole/FormController.php
+++ src/Controller/LaConsole/FormController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
-* @Route("/admin", name="form_")
+* @Route("/form", name="form_")
 */
 class FormController extends AbstractController{
     
